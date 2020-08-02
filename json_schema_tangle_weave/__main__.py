124d68591d029f719bee7fc876e1ba6d2bcb30ec
Stefan Hagen <stefan@hagen.link>
2020-08-02 19:54:29 +0200

    Create __main__.py

---
+++ json_schema_tangle_weave/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+import sys
+
+from json_schema_tangle_weave.cli import main
+
+if __name__ == "__main__":  # pragma: no cover
+    sys.exit(main(sys.argv[1:]))
