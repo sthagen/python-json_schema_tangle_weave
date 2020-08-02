5f2ce5b9afbe0207fc2c68fae6f1be44117080f5
Stefan Hagen <stefan@hagen.link>
2020-08-02 20:05:28 +0200

    Create json_schema_tangle_weave.py

---
+++ json_schema_tangle_weave/json_schema_tangle_weave.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+"""Add logical documentation here later TODO."""
+import json
+
+
+def weave(argv=None):
+    """Todo"""
+    pass
+
+
+def tangle(argv=None):
+    """Todo"""
+    pass
