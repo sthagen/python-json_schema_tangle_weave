ca32160e906831910b52603351d844a630a6a189
Stefan Hagen <stefan@hagen.link>
2020-12-25 19:32:10 +0100

    Moved no cover pragma to correct line.

--- json_schema_tangle_weave/__main__.py
+++ json_schema_tangle_weave/__main__.py
@@ -4,5 +4,5 @@ import sys
 
 from json_schema_tangle_weave.cli import main
 
-if __name__ == "__main__":  # pragma: no cover
-    sys.exit(main(sys.argv[1:]))
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))  # pragma: no cover
